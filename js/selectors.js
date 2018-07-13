68596834fcfdc71e708a0ebc538e466821ebed4c
Brad Schrag <brad.schrag@gmail.com>
2018-07-13 02:38:44 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  console.log($('div.pics ul#pic-list:first-child'))
+  return $('div.pics ul#pic-list:first-child')
+}
